fe1371bc04e11fefa751698bdeca0c4d1efe9daa
Googolplexed0 <65880807+Googolplexed0@users.noreply.github.com>
2025-01-21 02:48:04 -0500

    skip file read if unnecessary

--- zotify/track.py
+++ zotify/track.py
@@ -202,8 +202,9 @@ def download_track(mode: str, track_id: str, extra_keys=None, wrapper_p_bars: li
             filename = PurePath(filedir).joinpath(f'{filename.stem}_{c}{filename.suffix}')
         
         if Zotify.CONFIG.get_export_m3u8():
-            m3u_path = filedir / "Liked Songs.m3u8"
-            songs_m3u = fetch_m3u8_songs(m3u_path)
+            if mode == "liked" and Zotify.CONFIG.get_liked_songs_archive_m3u8():
+                m3u_path = filedir / "Liked Songs.m3u8"
+                songs_m3u = fetch_m3u8_songs(m3u_path)
             song_label = add_to_m3u8(mode, get_song_duration(track_id), song_name, filename, filedir)
             if mode == "liked" and Zotify.CONFIG.get_liked_songs_archive_m3u8():
                 if songs_m3u is not None and song_label in songs_m3u[0]:
