1d260f14cbe674685f31c4e0594713e5b798e970
Googolplexed0 <65880807+Googolplexed0@users.noreply.github.com>
2025-03-25 07:28:54 -0400

    remove unnecessary imports formatting

--- zotify/zotify.py
+++ zotify/zotify.py
@@ -1,10 +1,8 @@
 import json
 from pathlib import Path
-from pwinput import pwinput
 import datetime, time
 import requests
 from librespot.audio.decoders import VorbisOnlyAudioQuality
-from librespot.core import Session
 
 from zotify import OAuth, Session
 from zotify.const import TYPE, \
@@ -21,11 +19,11 @@ class Zotify:
         Zotify.CONFIG.load(args)
         Zotify.login(args)
         Zotify.datetime_launch = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
+    
     @classmethod
     def login(cls, args):
         """ Authenticates with Spotify and saves credentials to a file """
-
+    
         # Create session
         if args.username not in {None, ""} and args.token not in {None, ""}:
             oauth = OAuth(args.username)
@@ -48,17 +46,17 @@ class Zotify:
             cls.SESSION = Session.from_oauth(
                 oauth, cls.CONFIG.get_credentials_location(), cls.CONFIG.get_language()
             )
-
+    
     @classmethod
     def get_content_stream(cls, content_id, quality):
         return cls.SESSION.content_feeder().load(content_id, VorbisOnlyAudioQuality(quality), False, None)
-
+    
     @classmethod
     def __get_auth_token(cls):
         return cls.SESSION.tokens().get_token(
             USER_READ_EMAIL, PLAYLIST_READ_PRIVATE, USER_LIBRARY_READ, USER_FOLLOW_READ
         ).access_token
-
+    
     @classmethod
     def get_auth_header(cls):
         return {
@@ -67,7 +65,7 @@ class Zotify:
             'Accept': 'application/json',
             'app-platform': 'WebPlayer'
         }
-
+    
     @classmethod
     def get_auth_header_and_params(cls, limit, offset):
         return {
@@ -76,13 +74,13 @@ class Zotify:
             'Accept': 'application/json',
             'app-platform': 'WebPlayer'
         }, {LIMIT: limit, OFFSET: offset}
-
+    
     @classmethod
     def invoke_url_with_params(cls, url, limit, offset, **kwargs):
         headers, params = cls.get_auth_header_and_params(limit=limit, offset=offset)
         params.update(kwargs)
         return requests.get(url, headers=headers, params=params).json()
-
+    
     @classmethod
     def invoke_url(cls, url, tryCount=0):
         # we need to import that here, otherwise we will get circular imports!
@@ -94,17 +92,17 @@ class Zotify:
             responsejson = response.json()
         except json.decoder.JSONDecodeError:
             responsejson = {"error": {"status": "unknown", "message": "received an empty response"}}
-
+        
         if not responsejson or 'error' in responsejson:
             if tryCount < (cls.CONFIG.get_retry_attempts() - 1):
                 Printer.print(PrintChannel.WARNINGS, f"Spotify API Error (try {tryCount + 1}) ({responsejson['error']['status']}): {responsejson['error']['message']}")
                 time.sleep(5)
                 return cls.invoke_url(url, tryCount + 1)
-
+            
             Printer.print(PrintChannel.API_ERRORS, f"Spotify API Error ({responsejson['error']['status']}): {responsejson['error']['message']}")
-
+        
         return responsetext, responsejson
-
+    
     @classmethod
     def check_premium(cls) -> bool:
         """ If user has spotify premium return true """
